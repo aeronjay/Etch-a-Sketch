2f006b2bcd9465a8acd70921f5006d9baae1e152
Aeron Jay Bulatao <bulataoaeronjay@gmail.com>
2024-08-12 19:36:27 +0800

    add load grid function

---
+++ script.js
@@ -0,0 +1,20 @@
+const GRID_WIDTH_HEIGHT = 700;
+
+let grid = document.querySelector(".grid");
+
+function load(gridSize){
+    
+    for(let i = 0; i < gridSize; i++){
+        for(let j = 0; j < gridSize; j++){
+            grid.appendChild(createBlock(gridSize));
+        }
+    }
+}
+function createBlock(gridSize){
+    let blockSize = (GRID_WIDTH_HEIGHT / gridSize) - 1;
+    let block = document.createElement("div");
+    block.style.cssText = `flex: 1 0 ${blockSize + "px"}; background-color: black; height: auto;`;
+    
+    return block;
+}
+load(20);
